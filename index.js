4fc82abce1cc2f7b7201f899097a582226fb85a5
Jonathan Lee <kutsu89@gmail.com>
2024-12-09 12:22:07 +0000

    ticket 1

---
+++ index.js
@@ -0,0 +1,14 @@
+const titleElement = document.getElementById("title");
+console.log(titleElement);
+
+const contentElements = document.getElementsByClassName("content");
+console.log(contentElements);
+
+const liElements = document.getElementsByTagName("li");
+console.log(liElements);
+
+const firstHighlight = document.querySelector(".highlight");
+console.log(firstHighlight);
+
+const allHighlights = document.querySelectorAll(".highlight");
+console.log(allHighlights);
