5de80727aa857057f89d54a6bc8256af40fe97e0
sumeyacodes <sumeya4h@gmail.com>
2024-12-09 13:52:06 +0000

    ticket 2

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Ticket 1
 const titleElement = document.getElementById("title");
 console.log(titleElement);
 
@@ -12,3 +13,23 @@ console.log(firstHighlight);
 
 const allHighlights = document.querySelectorAll(".highlight");
 console.log(allHighlights);
+
+// Ticket 2
+//a
+let firstElement = document.querySelector("#itemList").firstElementChild;
+console.log(firstElement);
+
+let parentElement = firstElement.parentNode;
+console.log(parentElement);
+
+//b
+let ulElement = document.getElementById("itemList");
+console.log(ulElement.childNodes)
+
+//c
+
+console.log(ulElement.children);
+
+//d
+console.log(ulElement.firstChild, ulElement.lastElementChild)
+
